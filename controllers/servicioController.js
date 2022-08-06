7c17197e5d9c2a952a5f15eea3b2dc18a96bbdfd
Anahi <45857028+anluna@users.noreply.github.com>
2022-08-06 18:59:52 -0500

    Master

--- controllers/servicioController.js
+++ controllers/servicioController.js
@@ -11,3 +11,34 @@ exports.nuevoServicio = async(req,res, next) =>{
         next();
     }
 }
+
+exports.mostrarServicios = async(req, res) =>{
+    try {
+        const servicio = await Servicios.find({});
+        res.json(servicio);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+exports.boletasImpagas = async(req, res, next) =>{
+    const servicio = await Servicios.find({statusPago : req.params.statusPago});
+
+    if(!servicio){
+        res.json({mensaje : 'NO HAY BOLETAS IMPAGAS'});
+        return next()
+    }
+
+    res.json(servicio);
+}
+
+exports.boletasTipoServicio = async (req, res, next) => {
+    const servicio = await Servicios.find({tipoServicio : req.params.tipoServicio});
+    if(!servicio){
+        res.json({ mensaje: 'NO EXISTE EL SEEVICIO'});
+        return next()
+    }
+    res.json(servicio);
+    
+}
+
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,14 @@ module.exports = function (){
     //AGREGAR SERVICIOS POST 
     router.post('/servicios', servicioController.nuevoServicio);
 
+    //ENVIAR BOLETAS IMPAGAS
+    router.get('/servicios/:statusPago', servicioController.boletasImpagas);
+
+    //ENVIAR BOLETAS POR TIPO DE SERVICIO
+    router.get('/servicios/:tipoServicio', servicioController.boletasTipoServicio);
+
+    router.get('/servicios', servicioController.mostrarServicios);
+
     router.post('/pagos', pagosController.nuevoPago);
 
     return router;
